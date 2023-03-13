8f02871303a30a7147cfefef3bd74c35412bdb6a
JoseGomis299 <joseg03a@gmail.com>
2023-03-13 11:26:35 +0100

    Mejora Lobby online

--- PruebaTetris/Assets/LobbyOnline/Scripts/AuthenticateUI.cs
+++ PruebaTetris/Assets/LobbyOnline/Scripts/AuthenticateUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,41 @@ using UnityEngine.UI;
 public class AuthenticateUI : MonoBehaviour {
 
 
-    [SerializeField] private Button authenticateButton;
+    [SerializeField] private Button lobbyButton;
+    [SerializeField] private Button quickDrawButton;
+    
+    public static event Action onQuickJoin;
+    private bool quickJoin;
 
 
-    private void Awake() {
-        authenticateButton.onClick.AddListener(() => {
+    private void Awake()
+    {
+        LobbyListUI.onLeaveLobbyList += () => gameObject.SetActive(true);
+        
+        lobbyButton.onClick.AddListener(() =>
+        {
+            quickJoin = false;
             LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
             Hide();
         });
+        quickDrawButton.onClick.AddListener(() =>
+        {
+            quickJoin = true;
+            LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
+        });
+    }
+
+    private void Start()
+    {
+        LobbyManager.Instance.onAuthenticationComplete += StartGame;
+    }
+
+    private void StartGame()
+    {
+        if(!quickJoin) return;
+        LobbyManager.Instance.QuickJoinLobby();
+        onQuickJoin?.Invoke();
+        Hide();
     }
 
     private void Hide() {
