afc18b173eba308a6dbd3484a819f0291fe1a7af
JoseGomis299 <joseg03a@gmail.com>
2023-03-16 23:28:30 +0100

    Añadidas habilidades metal y rap

--- PruebaTetris/Assets/Scripts/Abilities/RapAbility.cs
+++ PruebaTetris/Assets/Scripts/Abilities/RapAbility.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class RapAbility : ICharacterAbility
 {
     public void UseAbility(PieceController myPieceController, PieceController enemyPieceController)
     {
-        throw new System.NotImplementedException();
+        if (NetworkManager.Singleton != null)
+        {
+            AbilitiesNetwork.Instance.PerformAbility(new NetworkObjectReference(myPieceController.NetworkObject),
+                new NetworkObjectReference(enemyPieceController.NetworkObject), 1);
+        }
+        else
+        {
+            int garbageCount = myPieceController.RemoveGarbage();
+            if(garbageCount == 0) return;
+
+            enemyPieceController.ThrowGarbage(garbageCount / 2, 0);
+        }
     }
 }
